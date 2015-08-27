2952246006ea79b793fb177eba67d97cf628a274
Milstein Munakami <milsonmun@yahoo.com>
2015-08-27 09:13:06 -0600

    updated localhost url

--- src/main/java/socialnetworking/library/Database.java
+++ src/main/java/socialnetworking/library/Database.java
@@ -9,7 +9,7 @@ public class Database {
 		try {
 			// String connectionURL =
 			// "jdbc:mysql://132.178.128.200:3306/db_socialnetwork";
-			String connectionURL = "jdbc:mysql://169.254.181.121:3306/db_socialnetwork";
+			String connectionURL = "jdbc:mysql://localhost:3306/db_socialnetwork";
 			java.sql.Connection connection = null;
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
 			// connection = DriverManager.getConnection(connectionURL,
